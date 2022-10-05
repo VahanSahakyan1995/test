6569a9c1d10f95acf82c4e22514542eab88d0a69
Vahan <vahan.sahakyan.23@gmail.com>
2022-10-05 20:03:40 +0400

    Fifth Commit

--- src/main/java/automated_tests/automated_tests_list/task/SecondTask.java
+++ src/main/java/automated_tests/automated_tests_list/task/SecondTask.java
@@ -4,26 +4,26 @@ import automated_tests.automated_tests_list.base.Base;
 import automated_tests.configuration.AppConfig;
 import automated_tests.utils.RetryAnalyzer;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
-import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
 
 public class SecondTask extends Base {
 
     @Test(retryAnalyzer = RetryAnalyzer.class)
     public void validateJSONSchema() {
-        RestAssured
-                .given()
-                .baseUri(AppConfig.getInstance().getRestAssuredUrl())
-                .contentType(ContentType.JSON)
-                .when()
-                .post()
-                .then()
-                .assertThat()
-                .statusCode(200)
-                .body("token", Matchers.notNullValue())
-                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schema.json"));
+        RestAssured.baseURI = AppConfig.getInstance().getRestAssuredUrl();
+        //obtain response
+        given()
+                .when().get()
+                //verify JSON Schema
+                .then().assertThat().statusCode(200)
+                .body(JsonSchemaValidator.
+                        matchesJsonSchema(new File("C:\\Users\\Admin\\Picsart Task\\test\\src\\main\\resources\\files\\schema.json")));
     }
 
 }
